d9f65cb93c4cde94e606c5c366409db5d39e3a84
Gilles Mathurin <gillesmath@me.com>
2009-05-08 10:53:12 -0400

    creation du controleur comments et implementation de ses actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,66 @@
+class CommentsController < ApplicationController
+  before_filter :find_article
+  
+  def index
+    @comments = @articles.comments
+  end
+
+  def show
+    @comment = @article.comments.find(params[:id])
+  end
+
+  def new
+    @comment = @article.comments.build
+  end
+
+  def edit
+    @comment = @article.comments.find(params[:id])
+  end
+  
+  def create
+    @comment = @article.comments.build(params[:comment])
+    @comment.author = current_user
+    
+    respond_to do |format|
+      if @comment.save
+        flash[:notice] = "Commentaire enregistré"
+        format.html { redirect_to article_path(@article) }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
+  
+  def update
+    @comment = @article.comments.find(params[:id])
+    @comment.author = current_user
+    
+    respond_to do |format|
+      if @comment.update_attributes(params[:comment])
+        flash[:notice] = "Modifications enregistrées"
+        format.html { redirect_to article_path(@article) }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
+  end
+  
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    
+    respond_to do |format|
+      if @article
+        format.html { redirect_to article_path(@artile) }
+      else
+        format.html { redirect_to comments_path() }
+      end
+    end
+  end
+  
+  private
+  
+  def find_article
+    @article = Article.find(params[:article_id]) if params[:article_id]
+  end
+end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,10 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe CommentairesController do
+describe CommentsController do
 
   #Delete these examples and add some real ones
-  it "should use CommentairesController" do
-    controller.should be_an_instance_of(CommentairesController)
+  it "should use CommentsController" do
+    controller.should be_an_instance_of(CommentsController)
   end
 
 
