abf680d8420d51353df724d4df4d427ea5a1c1a4
Gilles Mathurin <gillesmath@mac.com>
2009-05-10 08:46:32 -0400

    Ajout de l'attribut photo au modele Article
    creation du migration file pour gestion par le plugin paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090506005648) do
+ActiveRecord::Schema.define(:version => 20090509142552) do
 
   create_table "actions", :force => true do |t|
     t.string   "nom"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(:version => 20090506005648) do
     t.integer  "evenement_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "categorie_actions", :force => true do |t|
