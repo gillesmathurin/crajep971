fb9343ef12ab3d6789d8816c764cf9826b305412
Gilles Mathurin <gillesmath@me.com>
2009-05-11 09:17:17 -0400

    Ajout de la possibilité d'uploader une photo pour illustrer un article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   has_many :comments
   belongs_to :author, :class_name => "User", :foreign_key => "user_id"
   
-  has_attached_file :photo, :styles => { :thumbs => "75x150" }
+  has_attached_file :photo, :styles => { :thumb => "100x100" }
   
   named_scope :of_the_month, :conditions => ['created_at between ? and ?', Date.today.beginning_of_month, Date.today.end_of_month]
   
