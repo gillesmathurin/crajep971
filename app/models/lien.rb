4104a2e9f1e72cd2330cf187f12fb2016f6d578f
Gilles Mathurin <gillesmath@me.com>
2009-05-12 10:20:52 -0400

    ajout du placeholder des erreurs de validations de formulaire

--- app/models/lien.rb
+++ app/models/lien.rb
@@ -1,4 +1,4 @@
 class Lien < ActiveRecord::Base
-  validates_presence_of :nom, :adresse, :on => :create, :message => "can't be blank"
+  validates_presence_of :nom, :adresse, :on => :create, :message => "doit être rempli(e)"
   default_scope :order => 'created_at desc'
 end
