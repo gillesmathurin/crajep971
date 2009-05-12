3a70620e5e45a702fe1cedf7a7e75af37933f9e2
Gilles Mathurin <gillesmath@me.com>
2009-05-12 14:16:52 -0400

    Essai d'une autre configuration pour le mailer

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,24 @@
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = { 
+#   :address => "mail.451cra.lucian.hostingrails.com", 
+#   :port => 26, 
+#   :domain => "451cra.lucian.hostingrails.com",
+#   :authentication => :login, 
+#   :user_name => "postmaster+451cra.lucian.hostingrails.com", 
+#   :password => "glvMkDz95q7J"
+# }
+# ActionMailer::Base.perform_deliveries = true # TODO set to true for production
+# ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.default_charset = "iso-8859-1"
+
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = { 
-  :address => "mail.451cra.lucian.hostingrails.com", 
+  :address => "mail.crajep-temp.com", 
   :port => 26, 
-  :domain => "451cra.lucian.hostingrails.com",
+  :domain => "crajep-temp.com",
   :authentication => :login, 
-  :user_name => "postmaster+451cra.lucian.hostingrails.com", 
-  :password => "glvMkDz95q7J"
+  :user_name => "postmaster+crajep-temp.com", 
+  :password => "zFDLm4R1jKuT"
 }
 ActionMailer::Base.perform_deliveries = true # TODO set to true for production
 ActionMailer::Base.raise_delivery_errors = true
