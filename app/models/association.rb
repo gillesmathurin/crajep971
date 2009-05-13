167b1a2d7bc7eabcad899faf0552b47751f520a3
Gilles Mathurin <gillesmath@mac.com>
2009-05-13 18:01:52 -0400

    optimisation du modele Association

--- app/models/association.rb
+++ app/models/association.rb
@@ -2,7 +2,7 @@ class Association < ActiveResource::Base
   self.site = "http://www.asso-guadeloupe.fr"
   self.user = "gilles"
   self.password = "kagadew"
-  # self.timeout = 5
+  self.timeout = 5
   
   def self.find_the_crajep_ones
     ids_liste = [ 3, 1, 6, 9, 5, 11, 106, 13, 77, 82, 18, 8, 7, 12, 19, 644, 14, 10 ]
