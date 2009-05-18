6cbc9ab34d8e6fe10c91ccaae7769dade3d77d25
Gilles Mathurin <gillesmath@me.com>
2009-05-18 10:07:08 -0400

    Modification de l'initializer mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -20,3 +20,16 @@ ActionMailer::Base.default_charset = "utf-8"
 # ActionMailer::Base.perform_deliveries = true # TODO set to true for production
 # ActionMailer::Base.raise_delivery_errors = true
 # ActionMailer::Base.default_charset = "iso-8859-1"
+
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = { 
+#   :address => "mail.asso-guadeloupe.fr", 
+#   :port => 25, 
+#   :domain => "asso-guadeloupe.fr",
+#   :authentication => :login, 
+#   :user_name => "gilles+asso-guadeloupe.fr", 
+#   :password => "$rv+&c77ul&9"
+# }
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.raise_delivery_errors = false
+# ActionMailer::Base.default_charset = "iso-8859-1"
