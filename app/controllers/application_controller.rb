8acb1a3bb52e9655443491ae135b53c67844ff81
Gilles Mathurin <gillesmath@me.com>
2009-05-19 13:51:35 -0400

    mise en forme du code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
   def call_rake(task, options = {})
     options[:rails_env] ||= RAILS_ENV
     args = options.map do |n,v|
