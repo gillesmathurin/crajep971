26f61cb81739387582ac0f4e6cdd8e881ecb60e4
Gilles MATHURIN <gillesmath@macmini.local>
2010-06-01 10:31:08 -0400

    Freezed the app to Rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -53,4 +53,4 @@ Rails::Initializer.run do |config|
   config.i18n.default_locale = :fr
   require 'RedCloth'
 end
-ExceptionNotifier.exception_recipients = %w(gwadaman30@gmail.com gilles.math@me.com)
+ExceptionNotifier.exception_recipients = "gwadaman30@gmail.com"
